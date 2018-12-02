b729bc84847b5e2321e57772764912ad5dd6f778
Oran Gilboa <oran.gilboa1@gmail.com>
2018-12-01 23:01:38 -0800

    commit

--- project.py
+++ project.py
@@ -1,17 +1,17 @@
 
-# Requirements:
-# 0. python 2.7 installed
-# 1. install dependencies: "pip install image"
-
 import Tkinter as tk
 from PIL import Image, ImageTk
 root = tk.Tk()
 
+img = Image.open(r'nysunset.jpg')
+imgTk = ImageTk.PhotoImage(img)
+
 def motion(event):
     x, y = event.x, event.y
     print('{}, {}'.format(x, y))
-img = ImageTk.PhotoImage(Image.open(r'nysunset.jpg'))
-panel = tk.Label(root, image = img)
-panel.pack(side = "bottom", fill = "both", expand = "yes")
+    print(img.getpixel((x,y)))
+
+p = tk.Label(root, image = imgTk)
+p.pack(side = "bottom", fill = "both", expand = "yes")
 root.bind('<Motion>', motion)
 root.mainloop()
