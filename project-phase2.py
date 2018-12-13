3cdd78f823e51e5b6254c4a9f9bac7cf27780679
Oran Gilboa <oran.gilboa1@gmail.com>
2018-12-13 22:57:35 +0200

    push

---
+++ project-phase2.py
@@ -0,0 +1,65 @@
+
+import Tkinter as tk
+from PIL import Image, ImageTk
+import OSC
+
+root = tk.Tk()
+
+#FREQ_RANGE = (0, )
+DEGREE_RANGE = (0, 7)
+DURATION_RANGE = (0.2, 0.8)
+AMP_RANGE = (0.4, 3)
+
+img = Image.open(r'pics/nysunset.jpg')
+imgTk = ImageTk.PhotoImage(img)
+
+def click(event):
+    x, y = event.x, event.y
+    print('{}, {}'.format(x, y))
+    rgb = img.getpixel((x,y))
+    print(rgb)
+    makesound(*rgb)
+
+def get_value(range, num):
+	return range[0] + ((num / 255.0) * (range[1]-range[0]))
+
+def makesound(r, g, b):
+	degree = int(get_value(DEGREE_RANGE, r))
+	duration = get_value(DURATION_RANGE, g)
+	amp = get_value(AMP_RANGE, b)
+
+	client = OSC.OSCClient()
+	client.connect(('127.0.0.1', 57120))
+	msg = OSC.OSCMessage()
+	msg.setAddress("/makesound")
+	msg.append(degree)
+	msg.append(duration)
+	msg.append(amp)
+	client.send(msg)
+
+p = tk.Label(root, image = imgTk)
+p.pack(side = "bottom", fill = "both", expand = "yes")
+root.bind('<Button-1>', click)
+
+
+def hello():
+    print "hello!"
+
+menubar = tk.Menu(root)
+
+# create a pulldown menu, and add it to the menu bar
+filemenu = tk.Menu(menubar, tearoff=0)
+filemenu.add_command(label="Load image", command=hello)
+filemenu.add_command(label="Settings", command=hello)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+helpmenu = tk.Menu(menubar, tearoff=0)
+helpmenu.add_command(label="About", command=hello)
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+# display the menu
+root.config(menu=menubar)
+
+root.mainloop()
